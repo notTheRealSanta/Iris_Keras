636b66112e40f792bca9de6d5f83573abb1c8722
NotTheRealSanta <ucsantosh@gmail.com>
2017-08-16 10:57:18 +0530

    updated with prediction

--- iris_keras_main.py
+++ iris_keras_main.py
@@ -3,15 +3,19 @@ from keras.layers import Dense
 import numpy
 import pandas as pd
 
+# init a random seed
 numpy.random.seed(5)
 
+#reading iris dataset with pandas
 dataset = pd.read_csv('iris.csv', header = None)
+
+#Categorising the datesets to values and labels for training
 
 values = dataset.values[:,0:4].astype(float)
 labels_name = dataset.values[:,4:5]
 labels = numpy.zeros ( (150,3) )
-print labels[:1,].shape
 
+#one-hot encoding
 for x in range(0,150):
 	if ( labels_name[x]  == 'Iris-setosa') :
 		labels[x,0] = 1
@@ -20,22 +24,33 @@ for x in range(0,150):
 	else :
 		labels[x,2] = 1
 
+#taking the first row as test_data
+test_data = values[0]
 
-print ( values )
-print ( labels )
+values = numpy.delete ( values, (0), axis = 0)
+labels = numpy.delete ( labels , (0), axis = 0)
 
+#print ( values )
+#print ( labels )
+
+#building the model
 model = Sequential()
 model.add ( Dense ( 12, input_dim = 4, activation ='relu' ) )
 model.add ( Dense ( 3, activation = 'softmax' ) )
 
+#compling ( setup of the model )
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+#fit trains the model
 model.fit( values, labels, epochs=150, batch_size = 10)
 
+#evaluate returns the loss and mertric values ( i.e here the accuracy)
 scores = model.evaluate(values, labels)
+
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-print labels.shape
-
-prediction = model.predict(numpy.array([[5.1, 3.5, 1.4, 0.2]]))
-print prediction
+#prediction
+print ("Prediction for : ")
+print (test_data)
+prediction = model.predict(numpy.array([test_data]))
+print (prediction)
